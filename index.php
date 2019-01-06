38841350c45ff66f47978ce6f9cd6862095c5808
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-01-07 02:09:12 +0700

    Final Commit maybe

--- index.php
+++ index.php
@@ -1,33 +1,67 @@
 <?php
-    echo "<h1>Tilep elisa engine</h1><br><br>";
+    echo "<h1>Tilep Elisa Engine</h1><br><br>";
     require_once "elisa.php";
     require_once "googleDrive.php";
     require_once "database.php";
 
-    $indeks = 519202;
-    echo getIndeksTerakhir($conn) . "<br>";
-    incrementIndeks($conn);
-    echo getIndeksTerakhir($conn). "<br>";
-    echo isRunning($conn). "<br>";
+    date_default_timezone_set('Asia/Jakarta');
+    //cek supaya loop tidak dobel
+    $isRunning = isRunning($conn);
+    if($isRunning){
+        echo "Tilep sedang berjalan.<br>";
+        echo "Indeks sekarang: " . getIndeksTerakhir($conn);
+        exit();
+    }
+
+    echo "Jangan menutup browser hingga proses selesai.<br>";
+
+    //tiap run iterasi dibatasi
+    $iterasi = 10;
+    echo "Query sebanyak $iterasi kali <br><br>";
+
     setRunning($conn, true);
-    echo isRunning($conn). "<br>";
-    addTilepan($conn, 'Dummy name', 123, 'Dummy Id', 20190101);
-    /*
-    $file = downloadElisa($indeks);
-    
-    if($file === false){
-        echo "Gagal download $indeks";
-        //break;
-        exit();
+    $indeks = getIndeksTerakhir($conn);
+    $indeksGagal = 0;
+
+    for ($i=$iterasi; $i > 0; $i--) {
+        //memang indeks masih kosong
+        if($indeksGagal > 5){
+            break;
+        }
+        echo "<br><br><br>";
+        echo "Mencoba download $indeks <br>";
+        $file = downloadElisa($indeks);
+        
+        if($file === false){
+            echo "Gagal download $indeks <br>";
+            $indeksGagal++;
+            incrementIndeks($conn);
+            $indeks++;
+            continue;
+        }
+        echo "Berhasil download $indeks <br>";
+
+        $nama_file = $file['nama_file'];
+
+        echo "Mencoba upload $indeks $nama_file ke Google Drive <br>";
+        $fileId = uploadDrive($nama_file, $file['data_file']);
+
+        if($fileId === false){
+            echo "Gagal upload ke drive";
+            $indeksGagal++;
+            incrementIndeks($conn);
+            $indeks++;
+            continue;
+        }
+        echo "Berhasil upload $indeks $nama_file ke Google Drive dengan id $fileId <br>";
+
+        addTilepan($conn, $nama_file, $indeks, $fileId, date('Ymd'));
+        echo "File $indeks berhasil ditambahkan ke database. <br>";
+        incrementIndeks($conn);
+        $indeks++;
     }
-    
-    $fileId = uploadDrive($file['nama_file'], $file['data_file']);
 
-    if($fileId === false){
-        echo "Gagal upload ke drive";
-        exit();
-    }
-
-    var_dump($fileId);
-
+    setRunning($conn, false);
+    echo "<br><br><br>";
+    echo "Proses selesai. <br>"
 ?>
