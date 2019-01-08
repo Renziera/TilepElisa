b05eb857176338f0a02afdedb25f130b81df91e2
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-01-09 01:30:19 +0700

    Bisa limit tanggal
    Kalo query semua crash

--- searching/database.php
+++ searching/database.php
@@ -15,7 +15,7 @@
 
     function searchTilepan($conn, $katakunci){
         $katakunci = "%".$katakunci."%";
-        $searchSQL = 'SELECT nama_file, id FROM tilepanku WHERE nama_file LIKE :katakunci';
+        $searchSQL = 'SELECT nama_file, id, tanggal FROM tilepanku WHERE nama_file LIKE :katakunci';
         $query = $conn->prepare($searchSQL);
         $query->bindParam(':katakunci', $katakunci);
         $query->execute();
@@ -24,7 +24,25 @@
     }
 
     function limitTanggal($conn, $tanggalAwal, $tanggalAkhir){
+        $searchSQL = 'SELECT nama_file, id, tanggal FROM tilepanku WHERE tanggal >= :tanggalAwal AND tanggal <= :tanggalAkhir';
+        $query = $conn->prepare($searchSQL);
+        $query->bindParam(':tanggalAwal', $tanggalAwal);
+        $query->bindParam(':tanggalAkhir', $tanggalAkhir);
+        $query->execute();
+        $result = $query->fetchall();
+        return $result;
+    }
 
+    function searchNamaTanggal($conn, $katakunci, $tanggalAwal, $tanggalAkhir){
+        $katakunci = "%".$katakunci."%";
+        $searchSQL = 'SELECT nama_file, id, tanggal FROM tilepanku WHERE nama_file LIKE :katakunci AND tanggal >= :tanggalAwal AND tanggal <= :tanggalAkhir';
+        $query = $conn->prepare($searchSQL);
+        $query->bindParam(':katakunci', $katakunci);
+        $query->bindParam(':tanggalAwal', $tanggalAwal);
+        $query->bindParam(':tanggalAkhir', $tanggalAkhir);
+        $query->execute();
+        $result = $query->fetchall();
+        return $result;
     }
 
     function incrementSearchCount($conn){
--- searching/index.php
+++ searching/index.php
@@ -8,10 +8,10 @@
 <body>
     <form action="#tilep" method="post">
         <input type="text" name="search" placeholder="Masukkan kata kunci pencarian. . ." maxlength="30">
-        Batas upload 
-        <input type="date" name="date">
+        Batas upload dari
+        <input type="date" name="dateFrom">
         sampai
-        <input type="date" name="">
+        <input type="date" name="dateUntil">
         &nbsp;&nbsp;&nbsp;&nbsp;
         <input type="submit" value="Cari">
     </form>
@@ -20,36 +20,95 @@
             <tr>
                 <th>Nama file</th>
                 <th>Link download</th>
+                <th>Diupload tanggal</th>
             </tr>
        
 <?php
-    if(isset($_POST['search'])){
+    $need = true;
+    require_once "database.php";
+
+    function showResult($result){
+        if(sizeof($result) === 0){
+            echo "Tidak ditemukan.";
+        }
+
+        for ($i = sizeof($result) - 1; $i > -1 ; $i--) { 
+            echo "<tr>";
+            echo "<td>";
+            echo $result[$i]['nama_file'];
+            echo "</td>";
+            echo "<td>";
+            echo '<button type="button" onclick="window.open(\'http://adf.ly/20994703/https://renziera.web.id/tilepanelisa/?key=';
+            echo mt_rand(1000, 1000000) . 'QE8h'. mt_rand(1000000, 9999999) .'sda'. mt_rand(100, 999) .'&kleng=';
+            echo $result[$i]['id'];
+            echo '&sign=isekaiMipa\',\'_blank\')">Tilep</button>';
+            echo "</td>";
+            echo "<td>";
+            $tanggal = $result[$i]['tanggal'];
+            $tahun = substr($tanggal, 0, 4);
+            $bulan = substr($tanggal, 4, 2);
+            $hari = substr($tanggal, 6, 2);
+            echo $hari . '-' . $bulan . '-' . $tahun;
+            echo "</td>";
+            echo "</tr>";
+        }
+    }
+
+     if(!empty($_POST['dateFrom'])){
+        $dateFrom = $_POST['dateFrom'];
+        $dateFrom = str_replace('-', '', $dateFrom);
+        if(!is_numeric($dateFrom)){
+            $dateFrom = 0;
+        }
+
+        if(empty($_POST['search']) && empty($_POST['dateUntil'])){
+            $need = false;
+            $result = limitTanggal($conn, $dateFrom, 99999999);
+            showResult($result);
+        }
+    }
+
+    if(!empty($_POST['dateUntil']) && $need){
+        $dateUntil = $_POST['dateUntil'];
+        $dateUntil = str_replace('-', '', $dateUntil);
+        if(!is_numeric($dateUntil)){
+            $dateUntil = 99999999;
+        }
+
+        if(empty($_POST['search']) && empty($_POST['dateFrom'])){
+            $need = false;
+            $result = limitTanggal($conn, 0, $dateUntil);
+            showResult($result);
+        }
+
+        if(empty($_POST['search'])){
+            $need = false;
+            $result = limitTanggal($conn, $dateFrom, $dateUntil);
+            showResult($result);
+        }
+    }
+    if(!empty($_POST['search']) && $need){
         $search = $_POST['search'];
         if(strlen($search) < 3){
             echo "Query pencarian minimal 3 huruf.";
         }else{
-            require_once "database.php";
-            incrementSearchCount($conn);
-            $result = searchTilepan($conn, $search);
-            
-            if(sizeof($result) === 0){
-                echo "Tidak ditemukan.";
+            $need = false;
+            if(!empty($_POST['dateUntil']) && !empty($_POST['dateFrom'])){
+                $result = searchNamaTanggal($conn, $search, $dateFrom, $dateUntil);
+            }elseif(!empty($_POST['dateUntil'])){
+                $result = searchNamaTanggal($conn, $search, 0, $dateUntil);
+            }elseif(!empty($_POST['dateFrom'])){
+                $result = searchNamaTanggal($conn, $search, $dateFrom, 99999999);
+            }else{
+                $result = searchTilepan($conn, $search);
             }
-
-            for ($i = sizeof($result) - 1; $i > -1 ; $i--) { 
-                echo "<tr>";
-                echo "<td>";
-                echo $result[$i]['nama_file'];
-                echo "</td>";
-                echo "<td>";
-                echo '<button type="button" onclick="window.open(\'http://adf.ly/20994703/https://renziera.web.id/tilepanelisa/?key=QE8h328sda&kleng=';
-                echo $result[$i]['id'];
-                echo '&sign=isekaiMipa\',\'_blank\')">Tilep</button>';
-                echo "</td>";
-                echo "</tr>";
-            }
+            showResult($result);
         }  
-    }  
+    }
+
+    if($need === false){
+        incrementSearchCount($conn);
+    }
 ?>
         </table>
     </div>
